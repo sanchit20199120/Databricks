2bbebb6538affac21af8c9e000e678f2a7f9ef00
Sanchit <sanchit.bhard@gmail.com>
2024-01-05 15:22:12 -0500

    python basics

---
+++ python.py
@@ -0,0 +1,46 @@
+
+# -----------------INTRO TO PYTHON-----------------------------------------
+
+# Data Types ---------------------------
+'''
+DATA type in Python -
+1. Integer
+2. Float
+3. String  -- string can concate using + opertor
+4. Boolean  ---is a binary data type
+'''
+
+
+# print data type
+print(type(1.1))
+
+# integer and float
+print(1+1)
+print(1.1*2.3)
+
+# String Concat
+print("Hello" + " " + "Sanchit")
+
+# Variable -----------------------
+''' Variable names are case sensitive'''
+
+age = 32
+name = "Sanchit"
+print(name +"  "+ "age is" +" " +str(age))
+
+# f'string Formating ------------------------
+name = 'Sanchit'
+age = 32
+print(f"My name is {name} and I am {age} year old")
+
+# if else statement---------------------
+
+a = 0
+if a > 1:
+    print("a is greater than 1")
+elif a == 1:
+    print("a=1")
+else:
+    print("a is less than 1")
+
+
