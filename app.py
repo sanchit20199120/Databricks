2c627d4e40e3f11766a6d9357a24375026bd8f53
SANCHIT BHARDWAJ <71523734+sanchit20199120@users.noreply.github.com>
2024-01-30 16:42:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os   ---to get the runtime parameter
 from util import get_spark_session
 
-env= os.environ.get('ENVIRON')
-spark = get_spark_session(env, 'GitHub Activity - Getting Started')
 
-spark.sql('Select current_date').show()
+
+def main():
+    env= os.environ.get('ENVIRON')
+    spark = get_spark_session(env, 'GitHub Activity - Getting Started')
+    
+    spark.sql('Select current_date').show()
+
+
+if __main__ == '__main__':
+  main()
