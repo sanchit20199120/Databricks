79ac0106019a3f0f1fab28d71c453bb3edd8c7fe
SANCHIT BHARDWAJ <71523734+sanchit20199120@users.noreply.github.com>
2024-06-19 12:10:15 -0400

    Update python_code_practice.py

--- python_code_practice.py
+++ python_code_practice.py
@@ -1,1 +1,24 @@
+# count words in each line
 
+def count_words_in_line(file_path):
+  try:
+    
+    with open(file_path,'r') as file:
+      lines = file.readlines()  # will create a list of lines
+      print(lines)
+      for line in lines:
+        word_count = line.split(',')   # split the word in line 1 in list
+        print(word_count)
+      for index, count in enumerate(word_count):    
+        print(f"{index+1},len(count))
+  
+  except FileNotFoundError:
+    print(f" The file at {file_path} was not found.")
+
+
+count_words_in_line ('File_path')
+
+      
+    
+                        
+      
